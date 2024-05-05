e7c40441d7d483868666d1cf9c20045f0793f779
trayvonburl <157647616+trayvonburl@users.noreply.github.com>
2024-05-05 11:02:27 +0000

    Final Work

---
+++ slick/assets/js/scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 200,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 250
+    });
+
+
+});
